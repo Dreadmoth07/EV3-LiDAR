05cb68f7fac80483c05bc5a0951db59c2fb9c8ab
Louise Asif <404louise.a@gmail.com>
2025-10-25 15:59:30 +0100

    pre-chess commit, switching to seconds for movements

--- EV3/motors.py
+++ EV3/motors.py
@@ -6,11 +6,21 @@ from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
+def spinTableSpin():
+    spinTable = LargeMotor(OUTPUT_A)
+    spinTable.on_for_seconds(SpeedPercent(1), 20)
+
+def pivotMove():
+    upPivot = LargeMotor(OUTPUT_B)
+    upPivot.polarity = "inversed"
+    upPivot.on_for_seconds(SpeedPercent(1), 0.1)
+    upPivot.polarity = "normal"
+    
 
 def main():
-    spinTable = LargeMotor(OUTPUT_A)
-    spinTable.on_for_rotations(SpeedPercent(1), 1)
+    pivotMove()
     return 0
+    
 
 
 if __name__ == '__main__':
