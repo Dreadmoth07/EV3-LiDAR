89cc765db4cea0566a37942c8f6a538dba9d5909
Louise Asif <404louise.a@gmail.com>
2025-10-25 14:09:49 +0100

    adding all modules to ensure that we don't need to later

--- EV3/motors.py
+++ EV3/motors.py
@@ -1,3 +1,11 @@
+
+from time import sleep
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
+
 def main():
 
     return 0
--- EV3/sensors.py
+++ EV3/sensors.py
@@ -1,4 +1,10 @@
 
+from time import sleep
+
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+from ev3dev2.sensor import INPUT_1
+from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.led import Leds
 
 
 def main():
