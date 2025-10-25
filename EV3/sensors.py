439a13dc54fcc9c760e5a823002503e2d513dd08
progtato <kondiuchb@gmail.com>
2025-10-25 14:40:36 +0100

    Sensors V1

--- EV3/sensors.py
+++ EV3/sensors.py
@@ -1,14 +1,31 @@
-
+#!/usr/bin/env python3
 from time import sleep
 
 from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
 from ev3dev2.sensor import INPUT_1
-from ev3dev2.sensor.lego import TouchSensor
+from ev3dev2.sensor.lego import IRSensor
 from ev3dev2.led import Leds
 
+#                   array
+def calculatePoint(point);
+
+    return point
+
+def getDistance():
+    pass
+
+def rotatePoint(point, degrees):
+    pass
+
+def elevation():
+    pass
 
 def main():
-
+    point = [0,0,0]
+    distance = 0
+    rotation = 0
+    elevation = 0
+    printf("Hello World")
     return 0
 
 
